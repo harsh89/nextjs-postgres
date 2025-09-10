3f20077d14aed621a52ee34b629e878142b119ab
Harshvardhan Sharma <harshvardhansharma@harshvahansmbp2.home>
2025-09-09 22:44:18 -0400

    set type for prisma client

--- my-app/lib/prisma.js
+++ my-app/lib/prisma.js
@@ -3,7 +3,8 @@ import { PrismaClient } from '@prisma/client';
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+ prisma: PrismaClient = new PrismaClient();
+
 }  else {
     if(!global.prisma) {
         global.prisma = new PrismaClient();
